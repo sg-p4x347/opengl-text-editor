2efb529feb8e1d658f38c0d765bf5e97fa406830
Anthony Harris <33495441+KillerBOB999@users.noreply.github.com>
2019-11-09 20:14:16 -0600

    Added simple button class for GUI implementation of menu commands

---
+++ project2/Button.cpp
@@ -0,0 +1,61 @@
+#include "Button.h"
+
+Button::Button()
+{
+	setText("button");
+	setPosition(Vector2(0, 0));
+	setHeight(10);
+	setWidth(10);
+}
+
+Button::Button(string buttonText, Vector2 topLeftVertex, double height, double width)
+{
+	setText(buttonText);
+	setPosition(topLeftVertex);
+	setHeight(height);
+	setWidth(width);
+}
+
+Button::~Button()
+{
+}
+
+void Button::setText(string text)
+{
+	m_text = text;
+}
+
+string Button::getText()
+{
+	return m_text;
+}
+
+void Button::setPosition(Vector2 position)
+{
+	m_topLeftPosition = position;
+}
+
+Vector2 Button::getPosition()
+{
+	return m_topLeftPosition;
+}
+
+void Button::setHeight(double height)
+{
+	m_height = height;
+}
+
+double Button::getHeight()
+{
+	return m_height;
+}
+
+void Button::setWidth(double width)
+{
+	m_width = width;
+}
+
+double Button::getWidth()
+{
+	return m_width;
+}
