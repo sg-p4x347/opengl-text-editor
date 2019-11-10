0a7a54792bd85d6cbac5e28f1b94fc314268c56d
sg_p4x347 <sg.p4x347@gmail.com>
2019-11-10 15:00:58 -0600

    Major bug fix

--- project2/TextEditorWindow.h
+++ project2/TextEditorWindow.h
@@ -24,7 +24,7 @@ namespace ote {
 		uint32_t NearestCharacterIndex(vector<Word> row, int x);
 		void UpdateWords();
 		//void RenderWord(Word& word, Vector2& pen, size_t& rowHeight, uint32_t& charIndex, uint32_t& carat, int& windowWidth);
-		vector<Word> GenerateRow(queue<Word>& words);
+		vector<Word> GenerateRow(stack<Word>& words);
 		size_t RowCharacterCount(vector<Word>& row);
 		int CalculateRowHeight(vector<Word>& row);
 		void RenderRow(vector<Word>& row, Vector2 pen, uint32_t& charIndex, Document& document);
