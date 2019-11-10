f123737b802a3161f1b36641a967820d69c3f38a
sg_p4x347 <sg.p4x347@gmail.com>
2019-11-10 15:34:19 -0600

    Added help window

--- project2/Main.cpp
+++ project2/Main.cpp
@@ -17,33 +17,19 @@
 #include "pch.h"
 #include <GL/glut.h>				// include GLUT library
 #include "TextEditorWindow.h"
-#include "Bitmap.h"
-#include "Window.h"
-#include "Font.h"
-#include "FontUtil.h"
-#include "Color.h"
+#include "HelpWindow.h"
 
 int main(int argc, char ** argv) {
 	glutInit(&argc, argv);
 	
 	
 	
-	// create a generic help window
-	//auto helpWindow = Window::Create(std::make_shared<Window>("Help", Vector2(400, 0), Vector2(400, 200), 0, 1, 0, 1));
-	//helpWindow->SetRender([=]() {
-	//	// draw the background
-	//	glClear(GL_COLOR_BUFFER_BIT);
+	// create a help window
+	Window::Create(std::make_shared<HelpWindow>());
 
-	//	Vector2 position(100, 100);
-
-	//	FontUtil::Render(*helpWindow, position, "Hello World", "times", 12, Color());
-	//	// flush out the buffer contents
-	//	glFlush();
-
-	//});
 
 	// create a specialized text-editor window
-	auto textEditorWindow = Window::Create(std::make_shared<ote::TextEditorWindow>());
+	Window::Create(std::make_shared<ote::TextEditorWindow>());
 	glutMainLoop();
 	
 	return 0;
